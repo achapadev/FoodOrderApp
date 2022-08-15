90f34bccdf4455580f2d7698c6a294c01c4e2670
Adrian <achapa@adrianchapa.dev>
2022-08-15 03:34:42 -0500

    reducer logic to handle multiple of same item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -12,10 +12,24 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item)
-    // we use concat to return a new array instead of modifying existing state (immutable)
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    )
+    const existingCartItem = state.items[existingCartItemIndex]
+    let updatedItems
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
+    // we use concat to return a new array instead of modifying existing state (immutable)
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,

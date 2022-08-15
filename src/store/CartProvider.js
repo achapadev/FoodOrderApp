340a97f0743023c94f0eda9b439cfb4ef2647310
Adrian <achapa@adrianchapa.dev>
2022-08-15 10:50:52 -0500

    Items can now be incremented, decremented, and removed from Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -35,6 +35,29 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     }
   }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    )
+    const existingItem = state.items[existingCartItemIndex]
+    const updatedTotalAmount = state.totalAmount - existingItem.price
+    let updatedItems
+    if (existingItem.amount === 1) {
+      // remove item from new generated array because it is now less than 1
+      updatedItems = state.items.filter((item) => item.id !== action.id)
+    } else {
+      // don't remove item from array just update the amount
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+      updatedItems = [...state.items]
+      // create new array with the old items
+      updatedItems[existingCartItemIndex] = updatedItem
+      // overwrite item with updated item that has updated amount
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
   return defaultCartState
 }
 

24bcf22cf90d70c2aaf81f5b9b5aa230d8c831c4
Adrian <achapa@adrianchapa.dev>
2022-09-05 16:57:44 -0500

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,6 +8,7 @@ import classes from './AvailableMeals.module.css'
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
   // the function you pass to useEffect should not return a Promise
   // the function you pass to useEffect may return a cleanup function which can be executed
   // that cleanup function should run synchronously
@@ -18,6 +19,13 @@ const AvailableMeals = () => {
       const response = await fetch(
         'https://react-http-c03a9-default-rtdb.firebaseio.com/meals.json'
       )
+      if (!response.ok) {
+        // fetchMeals function will now throw error if something goes wrong with fetch request
+        // it is async function if we throw an error .. it will cause this promise to 'reject'
+        // can't use try/catch to wrap it unless we turned useEffect function into async which we cannot
+
+        throw new Error('Something went wrong!')
+      }
       const responseData = await response.json()
       // responseData will be an object but we want an array .. need to transform this
       const loadedMeals = []
@@ -32,13 +40,34 @@ const AvailableMeals = () => {
       setMeals(loadedMeals) //set Meals state using the loadedMeals helper constant
       setIsLoading(false)
     }
-    fetchMeals()
+
+    // use fact that it is promise and use catch method on it to handle errors / then method for success
+    fetchMeals().catch((error) => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
+
+    // try {
+    //   fetchMeals()
+    // } catch (error) {
+    //   setIsLoading(false)
+    //   // by default error is object with a message property
+    //   setHttpError(error.message)
+    // }
   }, [])
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
